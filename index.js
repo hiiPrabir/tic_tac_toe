67747ce5810a27c95bf266795e849d2bc62343ca
hiiPrabir <prabirsoren099@gmail.com>
2026-01-12 18:16:55 +0530

    Set default player names and implement resetGame function
    Replaced player prompts with default names and added resetGame function to clear boxes and reset game state.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 let turn = "X";
 let gameover=false;
-let playerA= prompt("Name of Player 1:");
-let playerB= prompt("Name of Player 2:");
+let playerA= "Player 1"
+let playerB= "Player 2";
+function resetGame() {
+    // 1. Select all boxes
+    let boxes = document.getElementsByClassName("box");
 
+    // 2. Loop through every box and clear the text
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].innerText = "";
+    }
+
+    // 3. Reset game variables
+    turn = "X";
+    gameover = false;
+}
 function handleClick(box) {
       function checkWin() {
     let box = document.getElementsByClassName("box");
+    let empty = false;
+
     if (
       (box[0].innerText !== "" &&
        box[0].innerText === box[1].innerText &&
@@ -39,11 +53,9 @@ function handleClick(box) {
 
       (box[2].innerText !== "" &&
        box[2].innerText === box[4].innerText &&
-       box[4].innerText === box[6].innerText)
-    ) {
+       box[4].innerText === box[6].innerText)) {
       return true;
     }
-
     return false;
   }
     if(box.innerText !== "" || gameover){
@@ -61,7 +73,7 @@ function handleClick(box) {
         }
         else{
             alert(playerA + " wins!");
-        }   
+        }
       gameover = true;
       return;
     }
