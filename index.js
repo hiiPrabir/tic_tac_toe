f8c4d2a66249d15aba6b936ca1f06e08e1509cb1
hiiPrabir <prabirsoren099@gmail.com>
2026-01-13 11:35:53 +0530

    Refactor game logic and add popup messages

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let turn = "X";
 let gameover=false;
 let playerA= "Player 1"
 let playerB= "Player 2";
+
+
 function resetGame() {
     // 1. Select all boxes
     let boxes = document.getElementsByClassName("box");
@@ -15,66 +17,107 @@ function resetGame() {
     turn = "X";
     gameover = false;
 }
-function handleClick(box) {
-      function checkWin() {
+function checkempty(){
     let box = document.getElementsByClassName("box");
-    let empty = false;
-
-    if (
-      (box[0].innerText !== "" &&
-       box[0].innerText === box[1].innerText &&
-       box[1].innerText === box[2].innerText) ||
-
-      (box[3].innerText !== "" &&
-       box[3].innerText === box[4].innerText &&
-       box[4].innerText === box[5].innerText) ||
-
-      (box[6].innerText !== "" &&
-       box[6].innerText === box[7].innerText &&
-       box[7].innerText === box[8].innerText) ||
-
-      // columns
-      (box[0].innerText !== "" &&
-       box[0].innerText === box[3].innerText &&
-       box[3].innerText === box[6].innerText) ||
-
-      (box[1].innerText !== "" &&
-       box[1].innerText === box[4].innerText &&
-       box[4].innerText === box[7].innerText) ||
-
-      (box[2].innerText !== "" &&
-       box[2].innerText === box[5].innerText &&
-       box[5].innerText === box[8].innerText) ||
-
-      // diagonals
-      (box[0].innerText !== "" &&
-       box[0].innerText === box[4].innerText &&
-       box[4].innerText === box[8].innerText) ||
-
-      (box[2].innerText !== "" &&
-       box[2].innerText === box[4].innerText &&
-       box[4].innerText === box[6].innerText)) {
-      return true;
+    for(let i=0;i<box.length;i++){
+        if(box[i].innerText===""){
+            return false;
+        }
     }
-    return false;
-  }
-    if(box.innerText !== "" || gameover){
+    return true;
+} 
+function openPopup(message) {
+  document.getElementById("popupMsg").innerText = message;
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+function restartFromPopup() {
+  closePopup();
+  resetGame();
+}
+function checkWin() {
+
+      let box = document.getElementsByClassName("box");
+      let empty = false;
+
+        if (
+          (box[0].innerText !== "" &&
+          box[0].innerText === box[1].innerText &&
+          box[1].innerText === box[2].innerText) ||
+
+          (box[3].innerText !== "" &&
+          box[3].innerText === box[4].innerText &&
+          box[4].innerText === box[5].innerText) ||
+
+          (box[6].innerText !== "" &&
+          box[6].innerText === box[7].innerText &&
+          box[7].innerText === box[8].innerText) ||
+
+          // columns
+          (box[0].innerText !== "" &&
+          box[0].innerText === box[3].innerText &&
+          box[3].innerText === box[6].innerText) ||
+
+          (box[1].innerText !== "" &&
+          box[1].innerText === box[4].innerText &&
+          box[4].innerText === box[7].innerText) ||
+
+          (box[2].innerText !== "" &&
+          box[2].innerText === box[5].innerText &&
+          box[5].innerText === box[8].innerText) ||
+
+          // diagonals
+          (box[0].innerText !== "" &&
+          box[0].innerText === box[4].innerText &&
+          box[4].innerText === box[8].innerText) ||
+
+          (box[2].innerText !== "" &&
+          box[2].innerText === box[4].innerText &&
+          box[4].innerText === box[6].innerText)) {
+          return true;
+        }
+      return false;
+      }
+
+function handleClick(box) {
+
+      if(box.innerText !== "" || gameover){
+          return;
+      }
+
+      box.innerText = turn;
+
+      if (turn === "X") {
+        turn = "O";
+
+      } else {
+
+        turn = "X";
+
+      }
+      if (checkWin()) {
+
+          if(turn==="X"){
+
+            openPopup(playerB + " wins! 🎉");
+            }
+
+            else{
+
+            openPopup(playerA + " wins!");
+
+          }
+
+          resetGame();
+
         return;
-    }
-    box.innerText = turn;
-    if (turn === "X") {
-      turn = "O";
-    } else {
-      turn = "X";
-    }
-    if (checkWin()) {
-        if(turn==="X"){
-            alert(playerB + " wins!");
-        }
-        else{
-            alert(playerA + " wins!");
-        }
-      gameover = true;
-      return;
-    }
+      }
+      if(checkempty()){
+          openPopup("It's a draw! 🤝");
+          resetGame();
+      }
   }
